fbe0532418b976efcf5b0f8147876437b011d1d7
Mouhamed Fall <faalen89@gmail.com>
2024-11-06 15:18:19 +0100

    update Jenkinsfile

--- pages/api/get-data.js
+++ pages/api/get-data.js
@@ -3,7 +3,7 @@ import db from '../../lib/db';  // Importe le client PostgreSQL
 
 export default async function handler(req, res) {
   try {
-    const result = await db.query('SELECT * FROM your_table');  // Exemple de requête
+    const result = await db.query('SELECT * FROM users');  // Exemple de requête
     res.status(200).json(result.rows);  // Renvoyer les résultats en JSON
   } catch (error) {
     console.error('Erreur lors de la connexion à la base de données :', error);
