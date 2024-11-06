c387f258ffa09befbd895d9745222d76d195a0a8
Mouhamed Fall <faalen89@gmail.com>
2024-11-06 14:35:10 +0100

    update Jenkinsfile

---
+++ pages/api/get-data.js
@@ -0,0 +1,12 @@
+// pages/api/get-data.js
+import db from '../../lib/db';  // Importe le client PostgreSQL
+
+export default async function handler(req, res) {
+  try {
+    const result = await db.query('SELECT * FROM your_table');  // Exemple de requête
+    res.status(200).json(result.rows);  // Renvoyer les résultats en JSON
+  } catch (error) {
+    console.error('Erreur lors de la connexion à la base de données :', error);
+    res.status(500).json({ error: 'Erreur interne du serveur' });
+  }
+}
